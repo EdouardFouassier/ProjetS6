36f019ae3c169a074f18424cfd530fac46532bcb
Laureline Martin <laureline.martin@ens.uvsq.fr>
2018-04-22 12:26:24 +0200

    Nouveau fichier : entree_sortie.h
    Ajout de la partie Validation

---
+++ Implementation/entree_sortie.h
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////
+/// Projet S6 - Sujet 4 - Algorithme Génétique ///
+///              entree_sortie.h               ///
+//////////////////////////////////////////////////
+
+
+#ifndef __ENTREE_SORTIE_H
+#define __ENTREE_SORTIE_H
+
+#include <string>
+
+// Validation
+bool testCoherenceDonnees(string nomFichier);
+bool estEntierPositif(auto valeur);
+bool estFloatPositif(auto valeur);
+bool estProbabilite(auto valeur);
+bool estString(auto valeur);
+bool estParsable(string fonction);
+
+
+
+#endif
